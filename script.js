d57128019df73e872aaea96ef9c51b01751fbac5
yassine grairi <grairiiyassine@gmail.com>
2026-01-21 02:44:28 +0100

    more optimizing and making undo / redo work

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-// getting elements from the html document
+
 const display = document.querySelector(".display-content")
 
 const numbersButtons = document.querySelectorAll("[data-number]")
@@ -14,31 +14,34 @@ const clearButton = document.querySelector("[data-clear]")
 
 const equalButton = document.querySelector("[data-equal]")
 
+const PInumberButton = document.querySelector("[data-number-pi]")
+
+const deleteButton = document.querySelector("#delete-btn")
+
+const previousCalcs = document.querySelector(".previous-calcs")
+
+const indoButton = document.querySelector("#indo")
+const redoButton = document.querySelector("#redo")
 
 
-const specialbtns = ["C", "√x", "±", "π", "=","%","."]
 
-const operators = {
-    "" : "",
-    "÷" : "÷",
-    "+" : "+",
-    "-" : "-",
-    "×" : "×",
-    "mod" : "mod",
-    "Xⁿ" : "^" }
+function createNewRecord(text){
+    let recordElement = document.createElement("p")
+    recordElement.textContent =  text
+    previousCalcs.appendChild(recordElement)
+}
 
-const errors = ["null", "undefined", "NaN", "Infinity", "-Infinity"]
 
 
 class Calculator{
     constructor(display){
         this.display = display
 
+        this.errors = ['undefined', 'null', 'NaN', 'Infinity', '-Infinity']
+
         this.firstOperand = ""
         this.secondOperand = ""
         this.operator = ""
-
-        this.record = ""
 
         this.calc = {
             "÷" : (a, b) => parseFloat(a) / parseFloat(b),
@@ -46,78 +49,292 @@ class Calculator{
             "-" : (a, b) => parseFloat(a) - parseFloat(b),
             "×" : (a, b) => parseFloat(a) * parseFloat(b),
             "mod" : (a, b) => parseFloat(a) % parseFloat(b),
-            "Xⁿ" : (a, b) => parseFloat(a) ** parseFloat(b),
+            "^" : (a, b) => parseFloat(a) ** parseFloat(b),
         }
 
         this.specialCalc = {
-            "√x" : (n) => Math.sqrt(n),
-            "±" : (n) => n * (-1),
-            "%" : (n) => n / 100,
+            "√x" : (n) => Math.sqrt(parseFloat(n)),
+            "±" : (n) => parseFloat(n) * (-1),
+            "%" : (n) => parseFloat(n) / 100,
         }
 
         this.switchOperand = false
-    }
-    updateDisplay(firstOperand = "", operator = "" , secondOperand = "", ){
-        this.display.textContent = firstOperand + operator + secondOperand
+        this.firstOperator = false
+        this.afterCalc = false
+        this.result = ""
+        this.record = []
     }
 
+    appendNumber(input){
+     
+        if (input === "." && this.firstOperand.includes(".") && !this.switchOperand) return
+        if (input === "." && this.secondOperand.includes(".") && this.switchOperand) return
+        if ( input === "." && !this.firstOperand){
+            this.firstOperand += "0."
+            return
+        }else if(input === "." && this.switchOperand && !this.secondOperand){
+            this.secondOperand += "0."
+            return
+        }
+        if (this.afterCalc){
+            this.firstOperand = input
+            this.afterCalc = false
+            return
+        }
+        if(!this.switchOperand){
+            this.firstOperand += input
+        }else{
+          this.secondOperand += input
+        } 
+    }
+    updateDisplay(result = undefined){
+        if (result) {
+            this.display.textContent = result
+            return
+        }
+        this.display.textContent = this.firstOperand + this.operator + this.secondOperand
+    }
 
+    newOperator(operatorEntred){
+    if (this.afterCalc){
+        this.operator = operatorEntred
+        this.switchOperand = true
+        this.afterCalc = false
+        return
+    }
+     if (this.switchOperand && this.secondOperand){
+        this.result = String(parseFloat(this.operate()))
+        this.firstOperand = this.result
+        this.operator = operatorEntred
+        this.secondOperand = ""
+     }else if (!this.switchOperand && this.firstOperand) {
+        this.operator = operatorEntred
+        this.switchOperand = true
+     }
+     else if(this.switchOperand && this.firstOperand){
+        this.operator = operatorEntred
+     }
+
+     
+    }
 
     clear() {
         this.firstOperand = ""
         this.secondOperand = ""
         this.operator = ""
         this.switchOperand = false
+        this.afterCalc = false
+        this.result = ""
+
     }
 
-    operate(firstOperand, operator, secondOperand) {
-        if (!operator || !secondOperand){
-            return firstOperand
+    operate() {
+        if (!this.operator || !this.secondOperand){
+            return this.firstOperand
         }
-        return this.calc[operator](firstOperand, secondOperand)
+        return this.calc[this.operator](this.firstOperand, this.secondOperand)
+    }
+
+    transform(spacialOperator){
+        if(this.switchOperand){
+            if(this.secondOperand){
+                this.secondOperand = String(this.specialCalc[spacialOperator](this.secondOperand))
+            }
+            
+        }else{
+            if(this.firstOperand){
+                this.firstOperand = String(this.specialCalc[spacialOperator](this.firstOperand))
+            }
+            
+        }
+        
+    }
+
+    removeDigit(){
+        if(!this.secondOperand && !this.operator){
+            this.firstOperand = this.firstOperand.slice(0, -1)
+        }else if(this.operator && !this.secondOperand){
+            this.operator = ""
+            this.switchOperand = false
+        }else{
+            this.secondOperand = this.secondOperand.slice(0, -1)
+        }
+    }
+
+    checkErrors(){
+        if(this.errors.includes(this.firstOperand) || this.errors.includes(this.operator)  || this.errors.includes(this.secondOperand) || this.errors.includes(this.result)) {
+            this.display.textContent = "ERROR" 
+            this.clear()
+        } 
+    }
+
+    calculatorResult(){
+        this.result = String(parseFloat(calculator.operate()))
+        if (!this.errors.includes(this.result)){
+            this.record.push([this.firstOperand , this.operator , this.secondOperand , this.result])
+            this.updateRecord()
+        }
+        
+        this.firstOperand = this.result
+        this.operator = ""
+        this.secondOperand = ""
+        this.switchOperand = false
+        this.afterCalc = true
+    }
+
+    updateRecord(){
+        createNewRecord(`${this.record.at(-1)[0]} ${this.record.at(-1)[1]} ${this.record.at(-1)[2]} = ${this.record.at(-1)[3]}`)
     }
 
    
 }
 
 
+
+
 const calculator = new Calculator(display)
 
+let undoIndex = calculator.record.length
+let alreadyIndo = false
+
+
+
+
+
+//number buttons
 numbersButtons.forEach(button => button.addEventListener("click", (event) => {
-    if (calculator.switchOperand){
-        calculator.secondOperand += event.target.textContent
-        calculator.updateDisplay(calculator.firstOperand, calculator.operator, calculator.secondOperand)
-    }else{
-        calculator.firstOperand += event.target.textContent
-        calculator.updateDisplay(calculator.firstOperand, calculator.operator, calculator.secondOperand)
-    }
+    calculator.appendNumber(event.target.textContent)
+    calculator.updateDisplay()
+    console.log(calculator.switchOperand,calculator.firstOperand,calculator.operator, calculator.secondOperand);
 }))
 
+
+// operations buttons
 operatorsButtons.forEach(button => button.addEventListener("click", (event) => {
-    calculator.operator = event.target.textContent
-    calculator.updateDisplay(calculator.firstOperand, calculator.operator, calculator.secondOperand)
-    if (calculator.firstOperand){
-        calculator.switchOperand = true
-    }
+    calculator.newOperator(event.target.textContent)
+    calculator.updateDisplay()
+    console.log(calculator.switchOperand,calculator.firstOperand,calculator.operator, calculator.secondOperand);
 } ))
 
+// special operations button
+spacialOperatorsButtons.forEach(button => button.addEventListener("click", (event) => {
+    calculator.transform(event.target.textContent)
+    calculator.updateDisplay()
+    calculator.checkErrors()
+    console.log(calculator.switchOperand,calculator.firstOperand,calculator.operator, calculator.secondOperand);
+}))
 
-equalButton.addEventListener("click", (event) => {
-    result = parseFloat(calculator.operate(calculator.firstOperand, calculator.operator, calculator.secondOperand))
-    calculator.updateDisplay(result)
-    switchOperand = false
+
+// equal button
+equalButton.addEventListener("click", () => {
+    calculator.calculatorResult()
+    calculator.updateDisplay(calculator.result)
+    calculator.checkErrors()
+    console.log(calculator.record);
+    undoIndex = (calculator.record.length - 1)
+    updateIndoRedo()
+    
 })
 
-
+// clear button
 clearButton.addEventListener("click", () => {
     calculator.clear()
     calculator.updateDisplay()
 })
 
 
+// pi button
+PInumberButton.addEventListener("click", () => {
+    calculator.switchOperand ? calculator.secondOperand = String(Math.PI) : calculator.firstOperand = String(Math.PI)
+    calculator.updateDisplay()
+}) 
+
+
+// delete button
+deleteButton.addEventListener("click", () =>{
+    calculator.removeDigit()
+    calculator.updateDisplay()
+    console.log(calculator.switchOperand,calculator.firstOperand,calculator.operator, calculator.secondOperand);
+})
 
 
 
+
+function restorePreviousOperations(){
+    calculator.firstOperand = calculator.record.at(undoIndex)[0]
+    calculator.operator = calculator.record.at(undoIndex)[1]
+    calculator.secondOperand = calculator.record.at(undoIndex)[2]
+}
+function indo(){
+    
+        if(!alreadyIndo){
+                calculator.clear()
+                restorePreviousOperations()
+                calculator.updateDisplay()
+                alreadyIndo = true
+                calculator.switchOperand = true
+                undoIndex -= 1
+        }else{
+                
+                if (undoIndex in calculator.record){
+                    calculator.clear()
+                    restorePreviousOperations()
+                    calculator.updateDisplay()
+                    calculator.switchOperand = true
+                    undoIndex -= 1
+
+                }
+        
+                
+    }
+    console.log(alreadyIndo)
+  
+    
+}
+
+function redo(){
+    undoIndex + 1 in calculator.record ? undoIndex += 1 : undoIndex = undoIndex
+    if(undoIndex in calculator.record){
+        calculator.clear()
+        restorePreviousOperations()
+        calculator.updateDisplay()
+        calculator.switchOperand = true
+    }
+    
+    console.log(alreadyIndo)
+}
+
+function updateIndoRedo(){
+      if (!alreadyIndo && undoIndex in calculator.record){
+        indoButton.classList.add("active")
+    }
+    else if ((alreadyIndo && undoIndex  in calculator.record)) {
+        indoButton.classList.add("active")
+    }else{
+        indoButton.classList.remove("active")
+    }
+
+    if (undoIndex + 1 in calculator.record){
+        redoButton.classList.add("active")
+    }else{
+        redoButton.classList.remove("active")
+    }
+}
+
+
+
+indoButton.addEventListener("click", () => {
+    indo()
+    updateIndoRedo()
+    console.log(undoIndex)
+    
+})
+
+redoButton.addEventListener("click", () => {
+    redo()
+    updateIndoRedo()
+    console.log(undoIndex)
+})
 
 
 
